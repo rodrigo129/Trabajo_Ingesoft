8210facfcf58dc5f6b4008338ef15529ef1957d9
Rodrigo <rodrigoignacio.vargas@alumnos.ulagos.cl>
2021-06-13 15:39:43 -0400

    plantilla de crear cuenta

---
+++ Capa_Vistas/Plantilla_Crear_Cuenta.php
@@ -0,0 +1,21 @@
+<?php
+require_once "Plantilla_barra.php"
+?>
+<body>
+<div class="container">
+    <!--<form action="Enviar_Datos.php" method="get">-->
+    <h3>
+        Datos del Cliente
+    </h3>
+    <div class="d-grid gap-3">
+        E-Mail: <input type="text" class="form-control" name="name" style="width: 300px">
+        Contraseña: <input type="text" class="form-control" name="name" style="width: 300px">
+        Nombre: <input type="text" class="form-control" name="name" style="width: 300px">
+        RUT: <input type="text" class="form-control" name="name" style="width: 300px">
+        Fono Contacto: <input type="text" class="form-control" name="name" style="width: 300px">
+
+    </div>
+    <input class="btn btn-primary" type="submit" value="Submit">
+</div>
+
+</body>
