758869a7c8612e2468c98936309ea254cd79c475
Rodrigo <rodrigoignacio.vargas@alumnos.ulagos.cl>
2021-06-13 15:38:56 -0400

    plantilla de barra dinamica

--- Capa_Vistas/Plantilla_barra.php
+++ Capa_Vistas/Plantilla_barra.php
@@ -1,4 +1,22 @@
 <?php
+
+function revisar_usuario(){
+    if ($_POST["Usuario"]==null){
+        echo "Iniciar sesion";
+    }
+    else{
+        echo $_POST["Usuario"];
+    }
+}
+function referencia_usuario(){
+    if ($_POST["Usuario"]==null){
+        echo "index.php?selecion=Iniciar_sesion";
+    }
+    else{
+        echo "index.php?selecion=Usuario";
+    }
+}
+
 ?>
 <nav class="navbar navbar-expand-xl bg-dark">
     <ul class="navbar">
@@ -12,7 +30,7 @@
             <a class="nav-link" href="#">link 1</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="index.php?selecion=Iniciar_sesion">Iniciar sesion</a>
+            <a class="nav-link" href="index.php?selecion=Iniciar_sesion"><?php revisar_usuario() ?></a>
         </li>
     </ul>
 
