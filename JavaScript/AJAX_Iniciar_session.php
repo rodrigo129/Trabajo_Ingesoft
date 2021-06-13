50de949ec38bfeea2fa0dd89cc2d5aca0886308e
Rodrigo <rodrigoignacio.vargas@alumnos.ulagos.cl>
2021-06-13 15:36:37 -0400

    js que envia solicitud de iniciar sesion

--- JavaScript/AJAX_Iniciar_session.php
+++ JavaScript/AJAX_Iniciar_session.php
@@ -1,1 +1,20 @@
 <?php
+?>
+<script>
+    function iniciar_sesion(){
+        var conexion = new XMLHttpRequest();
+        var user = document.getElementById("user").value
+        var password = document.getElementById("password").value
+        let mensaje = [user,password]
+        /*console.log("user="+user+",password="+password)*/
+        conexion.open("POST", "Capa_Control/control_inicio_sesion.php", true);
+        conexion.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        conexion.send("mensaje="+JSON.stringify(mensaje));
+        conexion.onreadystatechange = function (){
+            var respuesta = conexion.responseText;
+            /*document.cookie = "permisos="+String(respuesta)+"; max-age=60";
+            console.log(document.cookie)*/
+            console.log(respuesta)
+        }
+    }
+</script>
