81efd3d171cde168618e3bbb714b4a074d26c2c0
Rifo <48742868+LucasRifo@users.noreply.github.com>
2021-06-14 00:27:05 -0400

    Plantilla de Usuario creada

---
+++ Capa_Vistas/Plantilla_Usuario.php
@@ -0,0 +1,65 @@
+<head>
+    <meta charset="UTF-8">
+    <title>Trabajo Ingenieria de software</title>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1">
+    <style>
+        .parallax {
+            /* The image used */
+            background-image: url("../Imagenes/Malkuth Story.png");
+
+            /* Set a specific height */
+            height=100%;
+
+            /* Create the parallax scrolling effect */
+            background-attachment: fixed;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+    </style>
+    <link rel="stylesheet" href="../bootstrap-5.0.1-dist/css/bootstrap.min.css">
+    <script src="../bootstrap-5.0.1-dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<!--<script src="JavaScript/jquery.js"></script> -->
+<body>
+
+<div class="bg-image"
+     style="
+     background-image: url('../Imagenes/Malkuth Story.png');
+     background-attachment: fixed;
+     background-position: center;
+     background-repeat: no-repeat;
+     height: 200vh;
+  "
+>
+    <h1 class="text-warning">
+        Tienda Online
+    </h1>
+    <?php
+    #echo "plantilla inicio";
+    require_once "Plantilla_barra.php"
+    ?>
+    <h2 class=" text-warning" style="text-align: center">
+        Tu perfil
+    </h2>
+
+    <div class="container">
+        <!--<form action="Enviar_Datos.php" method="get">-->
+        <h2 class=" text-warning">
+            Datos del Cliente
+        </h2>
+        <div class="d-grid gap-3">
+            <p class=" text-warning">RUT: <!--Aqui la consulta--></p>
+            <p class=" text-warning">Nombre: <!--Aqui la consulta--></p>
+            <p class="text-warning">Apellido: <!--Aqui la consulta--></p>
+            <p class="text-warning">Titulo: <!--Aqui la consulta--></p>
+            <p class="text-warning">Permisos: <!--Aqui la consulta--></p>
+        </div>
+    </div>
+</div>
+
+</body>
+
+
+    </body>
+</div>
